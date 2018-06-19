9cb7304bf02cb7c1049904044c2df3dbfad11047
Gabrielle Saman <gabi.duarte@gmail.com>
2018-06-19 15:12:54 -0300

    Create count route to show how many cycles there are

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./billingCycle');
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true }) // returns new object and run validations also on PUT, not just POST.
 
+BillingCycle.route('count', (req, res) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+           res.status(500).json({ errors: [error]}) 
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
